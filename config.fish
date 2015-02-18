ec9597e35210276df6b8dba97f186fe8339bca52
Louis Borsu <sat@satprod.net>
2015-02-19 00:13:33 +0100

    Aliases

--- config.fish
+++ config.fish
@@ -1,7 +1,43 @@
 # Sublime text
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 
-# Greetings message
+# -------
+# Aliases
+# -------
+alias clr='clear'                         # Clear your terminal screen
+alias flush='killall -HUP mDNSResponder'  # Flush DNS (Yosemite)
+alias ip='curl icanhazip.com'             # Your public IP address
+alias ll='ls -al'                         # List all files in current directory in long list format
+alias ldir='ls -al | grep ^d'             # List all directories in current directory in long list format
+alias o='open .'                          # Open the current directory in Finder
+alias ut='uptime'                         # Computer uptime
+
+# ----------------------
+# Git Aliases
+# ----------------------
+alias ga='git add'
+alias gaa='git add .'
+alias gaaa='git add -A'
+alias gb='git branch'
+alias gbd='git branch -d '
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gcom='git checkout master'
+alias gd='git diff'
+alias gda='git diff HEAD'
+alias gi='git init'
+alias gl='git log'
+alias glg='git log --graph --oneline --decorate --all'
+alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
+alias gm='git merge --no-ff'
+alias gp='git pull'
+alias gss='git status -s'
+alias gst='git stash'
+alias gstl='git stash list'
+alias gstp='git stash pop'
+alias gstd='git stash drop'
 
 # Java
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_65.jdk/Contents/Home
